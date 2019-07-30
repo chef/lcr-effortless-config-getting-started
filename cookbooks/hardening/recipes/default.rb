a4769b52a6f69b91389e1312750993e5e282aa2e
John Snow <thelunaticscripter@outlook.com>
2019-07-30 06:34:48 -0400

    Add attributes overrides so windows works
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

--- cookbooks/hardening/recipes/default.rb
+++ cookbooks/hardening/recipes/default.rb
@@ -8,5 +8,8 @@ if node['os'] == 'linux'
   include_recipe 'os-hardening::default'
   include_recipe 'hardening::remediation'
 elsif node['os'] == 'windows'
+  node.default['security_policy']['rights']['SeNetworkLogonRight'] = nil
+  node.default['security_policy']['rights']['SeRemoteInteractiveLogonRight'] = nil
+  node.default['security_policy']['rights']['SeTrustedCredManAccessPrivilege'] = nil
   include_recipe 'windows-hardening'
 end
